781ca9a15ffac18459c0fb357701290599efd205
PiSloth <sologuardian.org@gmail.com>
2025-02-09 10:03:40 +0630

    Popular sale added

--- app/Livewire/BranchReport/Dashboard.php
+++ app/Livewire/BranchReport/Dashboard.php
@@ -3,7 +3,9 @@
 namespace App\Livewire\BranchReport;
 
 use App\Livewire\Order\Psi\DailySale;
+use App\Models\Branch;
 use App\Models\DailyReportRecord;
+use App\Models\PsiProduct;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\DB;
 use Livewire\Component;
@@ -15,12 +17,23 @@ class Dashboard extends Component
     public $month_filter;
     public $year_filter;
     public $report_types_date_filter;
+    public $ac = 'desc';
+    public $limit = 5;
+
+    public $branch_id;
+    public $popular_date_filter;
+
+    public $popular_month_filter;
+    public $popular_year_filter;
 
 
     public function mount()
     {
         $this->month_filter = Carbon::now()->month;
         $this->year_filter = Carbon::now()->year;
+
+        $this->popular_month_filter = Carbon::now()->month;
+        $this->popular_year_filter =  Carbon::now()->year;
 
 
         // dd($this->year_filter);
@@ -30,6 +43,13 @@ class Dashboard extends Component
     {
         $this->month_filter = Carbon::parse($value)->month;
         $this->year_filter = Carbon::parse($value)->year;
+    }
+
+    //sale popular data
+    public function updatedPopularDateFilter($value)
+    {
+        $this->popular_month_filter = Carbon::parse($value)->month;
+        $this->popular_year_filter = Carbon::parse($value)->year;
     }
 
     public function render()
@@ -75,6 +95,27 @@ class Dashboard extends Component
         //Branch index count Monthly
 
         //PSI Most popular sale
+        $most_popular = DB::table('real_sales as rs')
+            ->select('s.name as shape', 'p.length', 'p.weight', 'uoms.name as uom', DB::raw('SUM(rs.qty) AS sale'))
+            ->leftJoin('branch_psi_products as bpsi', 'rs.branch_psi_product_id', 'bpsi.id')
+            ->leftJoin('psi_products as p', 'p.id', 'bpsi.psi_product_id')
+            ->leftJoin('shapes as shp', 'shp.id', 'p.shape_id')
+            ->leftJoin('uoms', 'uoms.id', 'p.uom_id')
+            ->leftJoin('shapes as s', 's.id', 'p.shape_id')
+            ->leftJoin('branches as b', 'b.id', 'bpsi.branch_id')
+            ->where(function ($query) {
+                $query->whereMonth('rs.sale_date', $this->popular_month_filter)
+                    ->whereYear('rs.sale_date', $this->popular_year_filter);
+            })
+            ->when($this->branch_id, function ($query) {
+                return $query->where('b.id', $this->branch_id);
+            })
+            ->groupBy('s.name', 'p.length', 'uoms.name', 'p.weight')
+            ->orderByRaw('SUM(rs.qty)' . $this->ac)
+            ->limit($this->limit)
+            ->get();
+
+        // dd($most_popular);
 
         //Index by psi
 
@@ -89,6 +130,8 @@ class Dashboard extends Component
 
         return view('livewire.branch-report.dashboard', [
             'monthlyAllReportTypes' => $monthlyAllReportTypes,
+            'branches' => Branch::orderBy('name')->get(),
+            'sales' => $most_popular,
         ]);
     }
 }
--- resources/views/livewire/branch-report/dashboard.blade.php
+++ resources/views/livewire/branch-report/dashboard.blade.php
@@ -82,4 +82,65 @@
         <div class="p-2 mt-4 text-red-300 rounded-full bg-gray-50">No data found yet</div>
     @endif
 
+    {{-- Popular Sale --}}
+    <div class="mt-4">
+        <div class="w-1/2 mx-auto mb-4">
+            <x-datetime-picker wire:model.live.debounce="popular_date_filter" without-time='true' label="Date"
+                placeholder="Now" />
+        </div>
+        <div class="mb-4">
+            <span> Report at - {{ \Carbon\Carbon::parse($popular_date_filter)->format('M, Y') }}</span>
+            <select wire:model.live='branch_id' class="bg-gray-100 border rounded-lg border-gray-50">
+                <option value="" selected>All Branch</option>
+                @foreach ($branches as $branch)
+                    <option value="{{ $branch->id }}"> {{ ucfirst($branch->name) }}</option>
+                @endforeach
+            </select>
+            <select wire:model.live='ac' class="bg-gray-100 border rounded-lg border-gray-50">
+                <option value="desc" selected>အမြင့်ဆုံး</option>
+                <option value="asc">အနိမ့်ဆုံး</option>
+            </select>
+            <select wire:model.live='limit' class="bg-gray-100 border rounded-lg border-gray-50">
+                <option value="5" selected>5</option>
+                <option value="7">7</option>
+                <option value="10">10</option>
+                <option value="15">15</option>
+                <option value="20">20</option>
+            </select>
+        </div>
+
+        <table class="w-full border border-collapse border-gray-300 table-auto">
+            <thead>
+                <tr class="bg-gray-200">
+                    <th class="px-4 py-2 border border-gray-300">Rank</th>
+                    <th class="px-4 py-2 border border-gray-300">Shape</th>
+                    <th class="px-4 py-2 border border-gray-300">Sales</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($sales as $index => $item)
+                    <tr class="text-center {{ $index % 2 == 0 ? 'bg-gray-100' : '' }}">
+                        <td class="px-4 py-2 font-bold border border-gray-300">
+                            @if ($index == 0)
+                                👑
+                            @elseif ($index == 1)
+                                🥈
+                            @elseif ($index == 2)
+                                🥉
+                            @else
+                                {{ $index + 1 }}
+                            @endif
+                        </td>
+                        <td class="px-4 py-2 border border-gray-300">{{ $item->shape }}
+                            (<i class="text-slate-500"> {{ $item->weight }}
+                                {{ $item->length }}{{ $item->uom }}</i>)
+                        </td>
+                        <td class="px-4 py-2 font-semibold border border-gray-300">{{ number_format($item->sale) }}
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+
+    </div>
 </div>
